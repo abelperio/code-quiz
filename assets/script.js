9fa5b2ac2949dc906af0caebc6d61171b9882898
Alessia Belperio <abelperio.98@gmail.com>
2022-06-05 22:02:18 +0930

    set up of questions

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,13 @@
-// commonly used data types do not inclue: strings booleans alerts numbers
-// the contditions in an if/else statement are enclosed in: quotes, curly brackets, parentheses, square brackets
+const questions = ["Commonly used data types DO NOT include", "The conditions in an if/else statement are enclosed in","Arrays in javascript can be used to store", "String values must be enclosed in _____ when assigned when being assigned variables", "A very useful tool used during development and debugging for printing content to the debugger is"]
+const content = {
+    0: ["strings", "booleans", "alerts", "numbers"];
+    1: ["quotes", "curly brackets", "parentheses", "square brackets"];
+    2: ["numbers and strings", "other arrays", "booleans", "all of the above"];
+    3: ["commas", "curly brackets", "quotes", "parentheses"];
+    4: ["javascript", "terminal/bash", "for loops", "console.log"];
+}
+// commonly used data types do not include: strings booleans alerts numbers
+// the conditions in an if/else statement are enclosed in: quotes, curly brackets, parentheses, square brackets
 // arrays in javascript can be used to store: numbers and strings, other arrays, booleans, all of the above
 // string values must be enclosed in ___ when assigned when being assigned variables: commas, curly brackets, quotes, parentheses
 // a very useful tool used during development and debugging for printing content to the debugger is: javascript, terminal/bash, for loops, console.log 
